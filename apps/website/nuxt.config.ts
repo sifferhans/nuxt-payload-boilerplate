ae3bd1aabcb6e670de3d5626427b2caeb569d11e
Sigve Hansen <sighan12@gmail.com>
2023-02-09 16:13:04 +0100

    deps: add @nuxt/devtools

--- apps/website/nuxt.config.ts
+++ apps/website/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@vueuse/nuxt'],
+	modules: ['@vueuse/nuxt', '@nuxt/devtools'],
 	runtimeConfig: {
 		public: {
 			cmsUrl: 'http://localhost:3000'
